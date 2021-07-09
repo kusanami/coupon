8232f204170ac59af1dc613f812743e73c7d97ef
Laura Acosta <laura.acosta@payulatam.com>
2021-07-09 08:34:20 -0500

    test service

---
+++ application/src/test/java/com/coupon/restservice/controller/CouponControllerIntegrationTest.java
@@ -0,0 +1,13 @@
+package com.coupon.restservice.controller;
+
+/**
+ * @author <a href="mailto:laura.acosta@payu.com">Laura Acosta</a>
+ * @since 1.0.0
+ */
+public class CouponControllerIntegrationTest {
+
+    /*
+    1. Caso Exitoso
+    2. Caso 404
+     */
+}
